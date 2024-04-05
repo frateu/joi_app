db4cbecb9daaf44d149d54df87df2070f440c915
Matheus Freitas <matheus.fanjos@outlook.com>
2024-04-05 20:24:21 -0300

    Criação do menu sem muito estilo
    https://github.com/frateu/joi_app/issues/1

---
+++ lib/dadosPagina/tela_financeira.dart
@@ -0,0 +1,9 @@
+library tela_financeira;
+
+List<String> botoesFinanceira = [
+  'Botão 1',
+  'Botão 2',
+  'Botão 3',
+  'Botão 4',
+  'Botão 5',
+];
---
+++ lib/dadosPagina/tela_principal.dart
@@ -0,0 +1,7 @@
+library tela_principal;
+
+List<String> botoesPrincipal = [
+  'Botão 1',
+  'Botão 2',
+  'Botão 3',
+];
